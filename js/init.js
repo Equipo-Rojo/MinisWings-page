345c3aef97a5f843e31472fbad425e1dd738d5ef
DanielCornejoITM <danielcornejo1994@gmail.com>
2016-11-12 22:06:03 -0600

    Errores en el navbar

--- js/init.js
+++ js/init.js
@@ -4,9 +4,14 @@
 
     $('.button-collapse').sideNav();
 
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
  $(document).ready(function(){
       $('.slider').slider({full_width: true});
     });
+$('.button-collapse').sideNav({
+      menuWidth: 200 // Default is 240
+    }
+  );
