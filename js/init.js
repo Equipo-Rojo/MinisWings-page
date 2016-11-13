514fc5d3770f73d2b46bfbaa8e8e89ec8161d853
DanielCornejoITM <danielcornejo1994@gmail.com>
2016-11-12 19:34:42 -0600

    Cambios

--- js/init.js
+++ js/init.js
@@ -7,8 +7,6 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-//carousel
-  $('.carousel.carousel-slider').carousel({full_width: true});
  $(document).ready(function(){
       $('.slider').slider({full_width: true});
     });
