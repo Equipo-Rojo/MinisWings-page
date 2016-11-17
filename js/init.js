6d481137b92c14d0c6601622794b9a2697cbddd1
“DanielCornejoITM” <danielcornejo1994@gmail.com>
2016-11-16 21:10:35 -0600

    Agregue las salsas

--- js/init.js
+++ js/init.js
@@ -11,3 +11,17 @@
  $(document).ready(function(){
       $('.slider').slider({full_width: true});
     });
+
+function Muestra(n) {
+	var s= n-1;
+	LimpioImagen();
+	 var x = document.getElementsByClassName('comida');
+   x[s].style.display='block'
+
+
+
+
+}
+function LimpioImagen(){
+	 document.getElementById('elemento').style.display = 'none';
+}
